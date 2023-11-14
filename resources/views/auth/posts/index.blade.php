d1a63287065a8d3a103e68c60ead766d67c4e12e
Mohammadreza Ebrahimipur <mr.ebrahimipur@sepasholding.com>
2023-11-14 17:18:14 +0330

    []

--- resources/views/auth/posts/index.blade.php
+++ resources/views/auth/posts/index.blade.php
@@ -24,34 +24,45 @@
 
           <div class="col-lg-12 grid-margin stretch-card">
             <div class="card">
+
               <div class="card-body">
+                @if (count($posts) > 0)
                 <h4 class="card-title">Posts</h4>
+
                 <table class="table table-striped">
                   <thead>
                     <tr>
                       <th> Image </th>
                       <th> Title </th>
                       <th> Description </th>
-                      <th> Status </th>
+                      <th> Category </th>
                       <th> Action </th>
                     </tr>
                   </thead>
                   <tbody>
+                    @foreach ($posts as $post)
                     <tr>
-                      <td class="py-1">
-                        <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
-                      </td>
-                      <td> Herman Beck </td>
-                      <td>
-                        <div class="progress">
-                          <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
-                      </td>
-                      <td> $ 77.99 </td>
-                      <td> May 15, 2015 </td>
-                    </tr>
+                        <td class="py-1">
+                          <img src="../../assets/images/faces-clipart/pic-1.png" alt="image" />
+                        </td>
+                        <td> {{$post->title}} </td>
+                        <td>
+                            {{ Str::limit($post->description, 15, '...') }}
+                        </td>
+                        <td>  {{$post->category->name}}</td>
+
+                        <td> $ 77.99 </td>
+                        <td> May 15, 2015 </td>
+                      </tr>
+
+                    @endforeach
+
                   </tbody>
                 </table>
+
+                @else
+                <h3 class="text-center text-danger">No Post found</h3>
+                @endif
               </div>
             </div>
           </div>
